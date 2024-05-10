3505b1848cc8915b3af8f4250a9169e77db9aa05
surasith.kae <surasith.kae@ascendcorp.com>
2024-05-10 22:33:23 +0700

    feat: add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const httPrefix = process.env.HTTP_PREFIX;
+  app.setGlobalPrefix(httPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bariendo appointment API')
+    .setDescription('The Bariendo appointment API description')
+    .setVersion('1.0')
+    .addTag('appointment')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(httPrefix + '/swagger', app, document);
+
   const httpPort = process.env.HTTP_PORT || 3001;
   await app.listen(httpPort, () => {
     console.log(`Server is running on http://localhost:${httpPort}`);
